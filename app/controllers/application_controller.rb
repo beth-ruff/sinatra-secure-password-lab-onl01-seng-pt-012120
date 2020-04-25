9425abb4b8863a69914d76a3785578058e738a9d
Beth Ruff <beth.ruff2@gmail.com>
2020-04-25 15:56:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if user.save
+      redirect '/login'
+    else 
+      redirect '/failure'
+    end 
   end
 
   get '/account' do
